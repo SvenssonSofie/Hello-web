ac7eb5df7df280101c38365a5aa1f2ebcc192c73
Sofie Svensson <sofie.svensson@netlight.com>
2016-09-22 18:54:10 +0200

    added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000, function (){ 
+	console.log('The app is listening on http://localhost:3000')
+});
+
+app.get('/', function (req, res){
+
+	res.send('Hello web!')
+}) //specify route, navigate between webpages

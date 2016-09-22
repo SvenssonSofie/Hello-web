1b63f98192460f3a32bdc3e6fc18dc0b45d9362c
Sofie Svensson <sofie.svensson@netlight.com>
2016-09-22 20:09:56 +0200

    fancy images

---
+++ public/assets/js/app.js
@@ -0,0 +1,6 @@
+//custom js code for the browser to execute 
+//jquery often dollar sign - select different elements in html document
+$(document).ready(function(){
+	//custom js script
+	$('.materialboxed').materialboxed(); //all html in class materialboxed
+});
